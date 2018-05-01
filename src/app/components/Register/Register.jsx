cbd1c8693c96cd5aafe65802d7e49db642d32f34
Horn <HORNFRE000@mysbisd.org>
2018-05-01 14:33:55 -0500

    ds,mlk

--- src/app/components/Register/Register.jsx
+++ src/app/components/Register/Register.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './Register.scss'
 import { connect } from 'react-redux';
-import {checkEmail, checkUsername, submit} from '../../actions/register.actions';
-import {switchScreen} from '../../actions/homecontainer.actions';
+import { checkEmail, checkUsername, submit } from '../../actions/register.actions';
+import { switchScreen } from '../../actions/homecontainer.actions';
 
 class Register extends React.Component {
     constructor(props) {
@@ -13,32 +14,27 @@ class Register extends React.Component {
         let email;
         let password;
         let confirm_password;
-        console.log(this.props)
         return (
-            <div>
+            <div className={styles.cont}>
                 <form onSubmit={(e) => {
                     e.preventDefault();
                     this.props.dispatch(submit(username.value, email.value, password.value))
-                }}>
-                    <input type="text" placeholder="Username" required="true" ref={node=>username=node} onBlur={(e) => {
-                        // check username validity
-                        // onblur is fired when element loses focus
+                }} className={styles.from}>
+                    <input type="text" placeholder="Username" required="true" ref={node => username = node} onBlur={(e) => {
                         this.props.dispatch(checkUsername(username.value))
-                    }} />
-                    <input type="email" placeholder="johnsmith@gmail.com" required="true" ref={node=>email=node} onBlur={(e) => {
-                        // check email validity
+                    }} className={styles.username} />
+                    <input type="email" placeholder="Email" required="true" ref={node => email = node} onBlur={(e) => {
                         this.props.dispatch(checkEmail(email.value))
-                    }} />
-                    <input type="password" placeholder="Password" required="true" ref={node=>password=node}/>
-                    <input type="password" placeholder="Confirm Password" required="true" ref={node=>confirm_password=node} onBlur={(e)=>{
-                        // check that passwords match
-                        if(confirm_password.value!=password.value) alert('passwords must match')
-                    }}/>
-                    <input type="submit" value="Register!"/>
+                    }} className={styles.email} />
+                    <input type="password" placeholder="Password" required="true" ref={node => password = node} className={styles.pass} />
+                    <input type="password" placeholder="Confirm Password" required="true" ref={node => confirm_password = node} onBlur={(e) => {
+                        if (confirm_password.value != password.value) alert('passwords must match')
+                    }} className={styles.passConf} />
+                    <input type="submit" value="Register!" className={styles.submit} />
                 </form>
-                <button  onClick={e=>{
+                <a onClick={e => {
                     this.props.dispatch(switchScreen("login"));
-                }}> Login </button>
+                }} className={styles.login}> Login </a>
             </div>
         )
     }
