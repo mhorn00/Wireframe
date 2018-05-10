f0f22d3d02e79f565618d2788d6a1c39b77c5f15
Horn <HORNFRE000@mysbisd.org>
2018-05-10 14:08:41 -0500

    drop is not working now???????

--- src/app/components/FileList/Uploader/Uploader.jsx
+++ src/app/components/FileList/Uploader/Uploader.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { uploadState } from '../../../actions/filepage.actions';
+import { uploadState, updateProgress } from '../../../actions/filepage.actions';
 import styles from './Uploader.scss'
-import {URL as IP} from '../../../const';
+import { URL as IP } from '../../../const';
 
 class Uploader extends React.Component {
     constructor(props) {
@@ -12,8 +12,8 @@ class Uploader extends React.Component {
         this.onDragStopped = this.onDragStopped.bind(this);
     }
     onDrop(e) {
+        console.log('AHHHHHHHHHHHHHHH')
         e.preventDefault();
-        console.log('hello retard');
         for (var i = 0; i < e.dataTransfer.files.length; i++) {
             var data = new FormData();
             data.append('file', e.dataTransfer.files[i]);
@@ -26,6 +26,7 @@ class Uploader extends React.Component {
                     this.props.dispatch(uploadState('uploading'));
                 } else if (xhr.readyState == XMLHttpRequest.DONE) {
                     this.props.dispatch(uploadState('resting'));
+                    this.props.dispatch(updateProgress(0));
                 }
             };
             xhr.open('POST', `${IP}/upload`, true);
@@ -34,7 +35,7 @@ class Uploader extends React.Component {
                 if (e.total !== 0) {
                     progress = parseInt((e.loaded / e.total) * 100, 10);
                 }
-                //set the state to have the props
+                this.props.dispatch(updateProgress(progress));
             });
             xhr.send(data);
         }
@@ -54,9 +55,9 @@ class Uploader extends React.Component {
         return (
             <div className={styles.base}>
                 <p className={styles.text}>Drop Files Here</p>
-                <div onDrop={this.onDrop} onDragEnter={this.onDragStarted} onDragLeave={this.onDragStopped} className={styles[`${this.props.uploadState}`]} onDragOver={(e) => { e.preventDefault() }}>
+                <div onDrop={this.onDrop} onDragEnter={this.onDragStarted} onDragLeave={this.onDragStopped} className={styles[`${this.props.uploadState}`]} onDragOver={(e) => { console.log('HEWWO') }}>
                     <div className={styles.loading}>
-                        <p className={styles.text}>--%</p>
+                        <p className={styles.text}>{this.props.uploadProgress}</p>
                     </div>
                 </div>
             </div>
