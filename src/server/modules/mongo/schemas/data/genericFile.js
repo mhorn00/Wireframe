5ad5b248eb6217a6c4411b830aa845eed9752c6f
Adair Fulweber <adairf625@gmail.com>
2018-05-10 21:03:17 -0500

    we are going to fIX YOU

--- src/server/modules/mongo/schemas/data/genericFile.js
+++ src/server/modules/mongo/schemas/data/genericFile.js
@@ -3,12 +3,7 @@ var {Schema} = mongoose;
 
 let File = new Schema({
     absolutePath: String,
-    userRelativePath: {
-        type:[!String],
-        default: []
-    },
     rawName: String,
-    uploader: !String,
     name: !String,
     type: String,
     uploadDate:{
@@ -20,4 +15,37 @@ let File = new Schema({
     sharing_links: [String]
 })
 
-module.exports = mongo.model('File',File);
+let Folder = new Schema({
+    name: String,
+    path: [String],
+    owner: String,
+    name: String,
+    makeDate: {
+        type: Date,
+        default: Date.now()
+    },
+    children: [] // Mixed array so that both files and folders may be children 
+})
+
+let FolderModel = mongo.model('Folder',Folder);
+let FileModel = mongo.model('File', File)
+module.exports = {File:FileModel,Folder: FolderModel, FolderSchema: Folder};
+
+let Test = new FolderModel({
+    name: 'classic number 2',
+    path: [''],
+    owner: 'me bitch',
+})
+
+Test.save().then(()=>{
+    Test.children.push(new FolderModel({
+        name:'i am le nested foler 1',
+        children: [new FolderModel({
+            name: 'i am le nested foler number 2',
+            children: [new FileModel({
+                name: 'HI !!!'
+            })]
+        })]
+    }))
+    Test.save().then(()=>console.log('hey that is AWESOME'))
+})
